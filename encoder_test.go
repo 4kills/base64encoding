8a5c35d184fb98064ab8c5a94ab3a1c8392d6653
4kills <dominik.ochs@gmx.net>
2020-07-15 17:27:56 +0200

    added benchmarktests

---
+++ encoder_test.go
@@ -0,0 +1,25 @@
+package base64encoding
+
+import "testing"
+
+func TestNewCustom_PairwiseDistinction(t *testing.T) {
+	_, err := NewCustom(StandardCodeSet)
+	if err != nil {
+		t.Error(err)
+	}
+
+	// Easily readable code set with 2 runes alike
+	//                             |                                   |
+	//                             v                                   v
+	faulty := "*)23456789abcdefghi_klmnopqrstuvwxyzABCDEFGH+JKLMNOPQRSTkVWXYZ-$"
+	_, err = NewCustom(faulty)
+	if err != ErrNotDistinct {
+		t.Error(err)
+	}
+}
+
+func BenchmarkNewCustom(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		NewCustom(StandardCodeSet)
+	}
+}
