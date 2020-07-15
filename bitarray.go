9a750cf502e94754e3bc2399eb79445daa3987fe
4kills <dominik.ochs@gmx.net>
2020-07-15 22:34:44 +0200

    added bitarray

---
+++ bitarray.go
@@ -0,0 +1,63 @@
+package base64encoding
+
+import "errors"
+
+// BitArray is a fast, memory-efficient implementation of a bit (bool) array.
+// Use the NewBitArray function to create one.
+type BitArray struct {
+	size int
+	bits []byte
+}
+
+// NewBitArray creates and returns a new BitArray with the specified size
+func NewBitArray(size int) BitArray {
+	s := size / 8
+	if size%8 != 0 {
+		s++
+	}
+
+	b := make([]byte, s)
+	return BitArray{size, b}
+}
+
+// Len returns the len/size of the array
+func (b BitArray) Len() int {
+	return b.size
+}
+
+// Get returns the bit at the specified index.
+// false = 0. true = 1.
+func (b BitArray) Get(index int) bool {
+	checkInRange(b, index)
+
+	partition := b.bits[index/8]
+	offset := 0x80 >> (index % 8)
+
+	result := partition & byte(offset)
+	return result > 0
+}
+
+// Set allows you the set the bit at the specified index.
+// false = 0. true = 1.
+func (b BitArray) Set(index int, value bool) {
+	checkInRange(b, index)
+
+	partIdx := index / 8
+	partition := b.bits[partIdx]
+
+	val := 0x80 >> (index % 8)
+
+	if !value {
+		val = ^val
+		b.bits[partIdx] = partition & byte(val)
+		return
+	}
+
+	b.bits[partIdx] = partition | byte(val)
+}
+
+func checkInRange(b BitArray, index int) {
+	if index < 0 || b.size <= index {
+		panic(errors.New("bitarray: index out of range"))
+	}
+}
---
+++ bitarray_test.go
@@ -0,0 +1,19 @@
+package base64encoding
+
+import "testing"
+
+func BenchmarkGet(b *testing.B) {
+	bits := NewBitArray(1024)
+	size := bits.Len()
+	for i := 0; i < b.N; i++ {
+		bits.Get(i % size)
+	}
+}
+
+func BenchmarkSet(b *testing.B) {
+	bits := NewBitArray(1024)
+	size := bits.Len()
+	for i := 0; i < b.N; i++ {
+		bits.Set(i%size, true)
+	}
+}
